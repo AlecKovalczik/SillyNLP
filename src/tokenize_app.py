25a12019b95fdd29a058698a3178daa3efda3538
Alec Kovalczik <aleckovalczik@gmail.com>
2025-09-06 12:35:45 -0400

    Added very basic stop word filtration

--- src/tokenize_app.py
+++ src/tokenize_app.py
@@ -10,20 +10,32 @@ Command List:
 """
 
 import typer
+from typing import List
 from rich import print
 from nltk.tokenize import word_tokenize, sent_tokenize, regexp_tokenize
+from nltk.corpus import stopwords
 
 tokenize_app = typer.Typer(name="tokenize")
 
 @tokenize_app.command()
-def word(text):
+def word(text:str) -> None:
     """
     Tokenize the passed string based on words and symbols then return the result to stdout.
+
+    The resulting tokens are filtered based on a common set of stop words from nltk.
     
     Args:
         text (str): Text you want tokenized.
     """
-    print(word_tokenize(text))
+    words = word_tokenize(text)
+
+    # Note: stopwords includes the word "not" which we don't really want.
+    stop_words = set(stopwords.words("english"))
+    filtered_list = [
+        word for word in words if word.casefold() not in stop_words
+    ]
+
+    print(filtered_list)
 
 @tokenize_app.command()
 def sentence(text):
